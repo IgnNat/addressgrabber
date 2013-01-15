d38673229facd73d19ab9014db2c95598121eb28
kwazar23 <kwazar23@mail.ru>
2013-01-15 16:16:26 +0400

    Update index.php
    изменение title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=UTF-8');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Grabber Email</title>
+        <title>Email Grabber</title>
 
         <link rel="stylesheet" type="text/css" href="parser_main.css" media="all"/>
         <style type="text/css">
