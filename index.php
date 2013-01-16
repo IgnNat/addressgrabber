b94941ab2f71a4e7a7c2a4107cac2f4fed0f5e72
Игнатова Наталья <ignnatalja@gmail.ru>
2013-01-17 02:04:09 +0400

    замена стилей элементов select
    добавила библиотеки jquery.select.js и jsScroll.js
    добавила стили для новых селектов
    изменила сами элементы

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ header('Content-Type: text/html; charset=UTF-8');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Email Grabber</title>
+        <title>Grabber Email</title>
 
         <link rel="stylesheet" type="text/css" href="parser_main.css" media="all"/>
+        <link href="select.css" rel="stylesheet" type="text/css" />
         <style type="text/css">
 
 
@@ -60,7 +61,7 @@ header('Content-Type: text/html; charset=UTF-8');
             
             function SaveTextarea(){
                 //Сохраняем данные из textarea в DataArray
-                var text = document.getElementById("textarea").value;  
+                var text = document.getElementById("textarea").value;	
 						
                 var array = new Array();
                 var MyRegExp = /[\w-_А-Яа-я ]+/g;
@@ -207,7 +208,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
         <div id="main">
             <table width="100%" cellspacing="10" border="0">
-                <tr><td align="center" id="title" colspan="2"><h1>Email Grabber</h1></td></tr>
+                <tr><td align="center" id="title" colspan="2"><h1>Grabber Email</h1></td></tr>
                 <tr><td width="30%">
                         <h4 id="h4">Введите запросы, каждый с новой строки.</h4>
                     </td><td class="sett"><h4 id="h4">Настройки</h4>
@@ -218,21 +219,23 @@ header('Content-Type: text/html; charset=UTF-8');
                     <td class="sett">
                         <table>
                             <tr><td>Выберите глубину поиска:</td></tr>
-                            <tr><td><!--<input name="depth" id="depth" type="text" size="30" maxlength="3" onFocus="Focus('depth')" onBlur="Blur('depth','50')" value="50"/>-->
-                                    <select name="depth" id="depth">
-                                        <?php
-                                        for ($i = 1; $i <= 50; $i++) {
-                                            $val = $i * 10;
-                                            if ($i == 2) {
-                                                echo'<option value="' . $val . '" selected="selected">' . $val . '</option>';
-                                                continue;
-                                            }
-                                            echo'<option value="' . $val . '">' . $val . '</option>';
-                                        }
-                                        ?>
-                                    </select></td></tr>
+                             <tr><td><!--<input name="depth" id="depth" type="text" size="30" maxlength="3" onFocus="Focus('depth')" onBlur="Blur('depth','50')" value="50"/>-->
+
+                                           <select class="wid200" name="depth" id="depth">
+                                                <?php
+                                                for ($i = 1; $i <= 50; $i++) {
+                                                    $val = $i * 10;
+                                                    if ($i == 2) {
+                                                        echo'<option value="' . $val . '" selected="selected">' . $val . '</option>';
+                                                        continue;
+                                                    }
+                                                    echo'<option value="' . $val . '">' . $val . '</option>';
+                                                }
+                                                ?>
+                                            </select>
+                            </td></tr>
                             <tr><td>Выберите URL поисковика:</td></tr>
-                            <tr><td><select name="url" id="url">
+                            <tr><td><select class="wid100" name="url" id="url">
                                         <option value="google.ru">google.ru</option>
                                         <option value="google.com">google.com</option>
                                     </select>
@@ -266,6 +269,8 @@ header('Content-Type: text/html; charset=UTF-8');
         <script type="text/javascript" src="libraries/jquery-1.7.1.js"></script>	
         <script type="text/javascript" src="libraries/jquery.json-2.3.js"></script>
         <script type="text/javascript" src="libraries/heightDocument.js"></script>
+        <script type="text/javascript" src="libraries/select/jquery.selects.js"></script>
+        <script type="text/javascript" src="libraries/select/jsScroll.js"></script>
 
         <script type="text/javascript">
 
